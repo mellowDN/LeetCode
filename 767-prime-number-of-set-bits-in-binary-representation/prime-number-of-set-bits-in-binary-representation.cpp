0dc69f747931486757140eea6a9b9f1ceceb697f
mellowDN <164603880+mellowDN@users.noreply.github.com>
2025-07-01 11:09:15 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (99.54%) - LeetSync

--- 767-prime-number-of-set-bits-in-binary-representation/prime-number-of-set-bits-in-binary-representation.cpp
+++ 767-prime-number-of-set-bits-in-binary-representation/prime-number-of-set-bits-in-binary-representation.cpp
@@ -1,21 +1,10 @@
 class Solution {
 public:
-    int countSetBits(int n){
+    int countPrimeSetBits(int left, int right) {
         int count=0;
-        while(n){
-            n&=(n-1);
-            count++;
+        for(int i=left; i<=right; i++){
+            count+=665772>>__builtin_popcount(left++)&1;
         }
         return count;
-    }
-    int countPrimeSetBits(int left, int right) {
-        int track=0;
-        for(int i=left; i<=right; i++){
-            int set= countSetBits(i);
-            if(set==2 or set==3 or set==5 or set==7 or set==11 or set==13 or set==17 or set==19){
-                track++;
-            }
-        }
-        return track;
     }
 };
