87b098654c1a75fa898d77854a772001a1535f85
Parth More <164603880+mellowDN@users.noreply.github.com>
2026-01-14 21:21:53 +0530

    Time: 391 ms (20.89%) | Memory: 0B (100.00%) - LeetSync

--- 182-duplicate-emails/duplicate-emails.sql
+++ 182-duplicate-emails/duplicate-emails.sql
@@ -1,2 +1,2 @@
 # Write your MySQL query statement below
-SELECT distinct p1.email FROM Person as p1 INNER JOIN Person as p2 ON p1.email=p2.email AND p1.id!=p2.id;
+select email from(select email, count(*) as cnt from person group by email) as t where t.cnt>1;
